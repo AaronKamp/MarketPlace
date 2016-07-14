4f74b7797a0e3503f6146c2402615e88be8b77fb
ajithkhan <ajith@performixbiz.com>
2016-07-14 20:53:44 +0530

    Azure deployment scripts - ARM templates added.

---
+++ TCC Marketplace/DeploymentScripts/Scripts/Deploy-AzureResourceGroup.ps1
@@ -0,0 +1,159 @@
+﻿#Requires -Version 3.0
+#Requires -Module AzureRM.Resources
+#Requires -Module Azure.Storage
+
+Param(
+    [string] $ResourceGroupLocation = "East US",
+    [string] $ResourceGroupName = 'TCCMarketPlaceGroup',
+    [switch] $UploadArtifacts = $true,
+    [string] $StorageAccountName = "tccmarketplacestorage",
+    [string] $StorageAccountResourceGroupName = "TCCMarketPlaceGroup", 
+    [string] $StorageContainerName = 'tccmarketplace-stageartifacts',
+    [string] $TemplateFile = '..\Templates\TccMpDeploy.json',
+    [string] $TemplateParametersFile = '..\Templates\TccMpDeploy.parameters.json',
+    [string] $ArtifactStagingDirectory = '..\Artifacts',
+    [string] $AzCopyPath = '..\Tools\AzCopy.exe',
+    [string] $DSCSourceFolder = '..\DSC'
+)
+
+Import-Module Azure -ErrorAction SilentlyContinue
+
+try {
+    [Microsoft.Azure.Common.Authentication.AzureSession]::ClientFactory.AddUserAgent("VSAzureTools-$UI$($host.name)".replace(" ","_"), "2.8")
+} catch { }
+
+Set-StrictMode -Version 3
+
+$OptionalParameters = New-Object -TypeName Hashtable
+$TemplateFile = [System.IO.Path]::Combine($PSScriptRoot, $TemplateFile)
+$TemplateParametersFile = [System.IO.Path]::Combine($PSScriptRoot, $TemplateParametersFile)
+
+if ($UploadArtifacts) {
+    # Convert relative paths to absolute paths if needed
+    $AzCopyPath = [System.IO.Path]::Combine($PSScriptRoot, $AzCopyPath)
+    $ArtifactStagingDirectory = [System.IO.Path]::Combine($PSScriptRoot, $ArtifactStagingDirectory)
+    $DSCSourceFolder = [System.IO.Path]::Combine($PSScriptRoot, $DSCSourceFolder)
+
+    Set-Variable ArtifactsLocationName '_artifactsLocation' -Option ReadOnly -Force
+    Set-Variable ArtifactsLocationSasTokenName '_artifactsLocationSasToken' -Option ReadOnly -Force
+  
+    $OptionalParameters.Add($ArtifactsLocationName, $null)
+    $OptionalParameters.Add($ArtifactsLocationSasTokenName, $null)
+
+
+
+    # Parse the parameter file and update the values of artifacts location and artifacts location SAS token if they are present
+    $JsonContent = Get-Content $TemplateParametersFile -Raw | ConvertFrom-Json
+    $JsonParameters = $JsonContent | Get-Member -Type NoteProperty | Where-Object {$_.Name -eq "parameters"}
+
+    if ($JsonParameters -eq $null) {
+        $JsonParameters = $JsonContent
+    }
+    else {
+        $JsonParameters = $JsonContent.parameters
+    }
+
+    $JsonParameters | Get-Member -Type NoteProperty | ForEach-Object {
+        $ParameterValue = $JsonParameters | Select-Object -ExpandProperty $_.Name
+
+
+        if ($_.Name -eq $ArtifactsLocationName -or $_.Name -eq $ArtifactsLocationSasTokenName) {
+            $OptionalParameters[$_.Name] = $ParameterValue.value
+            
+        }
+    }
+
+     #Create or check for existing resource group
+    $resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+    if(!$resourceGroup)
+    {
+        Write-Host "Resource group '$resourceGroupName' does not exist. To create a new resource group, please enter a location.";
+        if(!$resourceGroupLocation) {
+            $resourceGroupLocation = Read-Host "resourceGroupLocation";
+        }
+        Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'";
+        New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
+    }
+    else{
+        Write-Host "Using existing resource group '$resourceGroupName'";
+    }
+
+     #Create or check for existing Storage account
+    $storageAccount = Get-AzureRmStorageAccountKey -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccountName -ErrorAction SilentlyContinue
+    if(!$storageAccount)
+    {
+        Write-Host "Storage account '$StorageAccountName' does not exist. To create a new storage account, please enter a location.";
+        if(!$ResourceGroupLocation) {
+            $ResourceGroupLocation = Read-Host "resourceGroupLocation";
+        }
+        Write-Host "Creating storage account '$StorageAccountName' in location '$resourceGroupLocation'";
+        New-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -AccountName $StorageAccountName -Location $ResourceGroupLocation -Type "Standard_GRS"
+    }
+    else{
+        Write-Host "Using existing storage account '$StorageAccountName'";
+    }
+
+   
+
+
+    $StorageAccountKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccountName)[0].Value 
+       
+    $StorageAccountContext = (Get-AzureRmStorageAccount -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccountName).Context
+   
+    # Create DSC configuration archive
+    if (Test-Path $DSCSourceFolder) {
+        Add-Type -Assembly System.IO.Compression.FileSystem
+        $ArchiveFile = Join-Path $ArtifactStagingDirectory "dsc.zip"
+        Remove-Item -Path $ArchiveFile -ErrorAction SilentlyContinue
+        [System.IO.Compression.ZipFile]::CreateFromDirectory($DSCSourceFolder, $ArchiveFile)
+    }
+
+    # Generate the value for artifacts location if it is not provided in the parameter file
+    $ArtifactsLocation = $OptionalParameters[$ArtifactsLocationName]
+    if ($ArtifactsLocation -eq $null) {
+        $ArtifactsLocation = $StorageAccountContext.BlobEndPoint + $StorageContainerName <# "/MarketPlaceDeployment"#>
+        $OptionalParameters[$ArtifactsLocationName] = $ArtifactsLocation
+    }
+    Write-Host $OptionalParameters[$ArtifactsLocationName]
+    Write-Host "Storage container creation - Started "
+
+    $StorageContext = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
+
+    $blobContainer = Get-AzureStorageContainer -Name $StorageContainerName -Context $StorageContext -ErrorVariable errContainer -Verbose:$false -ErrorAction "SilentlyContinue"
+
+    if($blobContainer -eq $null)
+    {
+        New-AzureStorageContainer -Name $StorageContainerName -Context $StorageContext -Verbose | Out-Null
+    }
+    Write-Host "Storage container creation - Completed "
+
+    # Use AzCopy to copy files from the local storage drop path to the storage account container
+    & $AzCopyPath """$ArtifactStagingDirectory""", $ArtifactsLocation, "/DestKey:$StorageAccountKey", "/S", "/Y", "/Z:$env:LocalAppData\Microsoft\Azure\AzCopy\$ResourceGroupName"
+    if ($LASTEXITCODE -ne 0) { return }
+
+    # Generate the value for artifacts location SAS token if it is not provided in the parameter file
+    $ArtifactsLocationSasToken = $OptionalParameters[$ArtifactsLocationSasTokenName]
+   
+    if ($ArtifactsLocationSasToken -eq $null) {
+        # Create a SAS token for the storage container - this gives temporary read-only access to the container
+        $ArtifactsLocationSasToken = New-AzureStorageContainerSASToken -Container $StorageContainerName -Context $StorageAccountContext -Permission r -ExpiryTime (Get-Date).AddHours(4)
+        $ArtifactsLocationSasToken = ConvertTo-SecureString $ArtifactsLocationSasToken -AsPlainText -Force
+        $OptionalParameters[$ArtifactsLocationSasTokenName] = $ArtifactsLocationSasToken
+    }
+ 
+}
+
+# Create or update the resource group using the specified template file and template parameters file
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -Force -ErrorAction Stop 
+
+New-AzureRmResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
+                                   -ResourceGroupName $ResourceGroupName `
+                                   -TemplateFile $TemplateFile `
+                                   -TemplateParameterFile $TemplateParametersFile `
+                                   @OptionalParameters `
+                                   -Force -Verbose
+                                  
+                                  
+
+								 
+                                   
